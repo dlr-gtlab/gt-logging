6a4ccd232cf773c48bc24ebb4cf4768475afcc70
Reitenbach, Stanislaus <stanislaus.reitenbach@dlr.de>
2019-10-23 15:44:05 +0200

    Revert "test commit"
    This reverts commit 12022069e35ed21527f4ac38f343369c007196db

--- src/QsLog.cpp
+++ src/QsLog.cpp
@@ -52,7 +52,6 @@ static const char FatalString[] = "FATAL";
 
 // not using Qt::ISODate because we need the milliseconds too
 //static const QString fmtDateTime("yyyy-MM-ddThh:mm:ss.zzz");
-// bla blup
 static const QString fmtDateTime("[hh:mm:ss]");
 
 static const char* LevelToText(Level theLevel)
