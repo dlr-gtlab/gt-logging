c71501bd950dcd2402c9ba82765e2d01f1b74820
Schmeink, Jens <jens.schmeink@dlr.de>
2020-03-03 09:23:49 +0100

    Resolve "Build Tests for Qt5.12 system"

---
+++ features/gitlab_windows_local_settings-nightly512.pri
@@ -0,0 +1,17 @@
+#             ______________      __
+#            / ____/_  __/ /___  / /_
+#           / / __  / / / / __ `/ __ \
+#          / /_/ / / / / / /_/ / /_/ /
+#          \____/ /_/ /_/\__,_/_.___/
+
+BUILD_UNITTESTS =   false
+
+# path to dev tools
+DEV_TOOLS = C:\devel\GTlab-DevTools
+
+
+# THIRD PARTY
+GOOGLE_TEST_PATH = $${DEV_TOOLS}\ThirdPartyLibraries\GoogleTest
+
+# Deployment
+GTLAB_ENVIRONMENT_PATH =
---
+++ tests/build/copyHeaders512.bat
@@ -0,0 +1,8 @@
+SET NIGHTLYBUILD=G:\AT-TW\GTlab\Nightly_Builds_512
+
+if not exist "%NIGHTLYBUILD%\include\logging\" mkdir %NIGHTLYBUILD%\include\logging\
+if not exist "%NIGHTLYBUILD%\lib\logging\" mkdir %NIGHTLYBUILD%\lib\logging\
+
+for /r .\include %%f in (*.h) do @copy "%%f" %NIGHTLYBUILD%\include\logging\
+for /r .\lib %%f in (*.dll) do @copy "%%f" %NIGHTLYBUILD%\lib\logging\
+for /r .\lib %%f in (*.lib) do @copy "%%f" %NIGHTLYBUILD%\lib\logging\
