a0eae87bd8cf52b73e37a5db7ad4f222ae72f316
Nöthen <Marvin.Noethen@dlr.de>
2020-05-06 16:19:20 +0200

    deployment is updated to version 1.3

--- tests/build/copyHeaders512.bat
+++ tests/build/copyHeaders512.bat
@@ -1,6 +1,8 @@
 SET NIGHTLYBUILD=G:\AT-TW\GTlab\Nightly_Builds_512
 
+if exist "%NIGHTLYBUILD%\include\logging\" rmdir /s /q %NIGHTLYBUILD%\include\logging\
 if not exist "%NIGHTLYBUILD%\include\logging\" mkdir %NIGHTLYBUILD%\include\logging\
+if exist "%NIGHTLYBUILD%\lib\logging\" rmdir /s /q %NIGHTLYBUILD%\lib\logging\
 if not exist "%NIGHTLYBUILD%\lib\logging\" mkdir %NIGHTLYBUILD%\lib\logging\
 
 for /r .\include %%f in (*.h) do @copy "%%f" %NIGHTLYBUILD%\include\logging\
