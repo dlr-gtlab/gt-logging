3a8a2606e371dad412d733add9cc135c4a98f1ab
Nöthen, Marvin <marvin.noethen@dlr.de>
2020-11-18 17:01:45 +0100

    Resolve "Pipeline adjustment"

--- tests/build/copyHeaders-installer.bat
+++ tests/build/copyHeaders-installer.bat
@@ -5,10 +5,10 @@ if not exist "%SERVERPATH%\include\%TARGETDIRNAME%\" mkdir %SERVERPATH%\include\
 if exist "%SERVERPATH%\lib\%TARGETDIRNAME%\" rmdir /s /q %SERVERPATH%\lib\%TARGETDIRNAME%\
 if not exist "%SERVERPATH%\lib\%TARGETDIRNAME%\" mkdir %SERVERPATH%\lib\%TARGETDIRNAME%\
 
-if not exist "%SERVERPATH%\bin\modules\" mkdir %SERVERPATH%\bin\modules\
-if not exist "%SERVERPATH%\binDebug\modules\" mkdir %SERVERPATH%\binDebug\modules\
+if not exist "%SERVERPATH%\%BINDIRECTORY%" mkdir %SERVERPATH%\%BINDIRECTORY%\
+if not exist "%SERVERPATH%\%DEBUGBINDIRECTORY%\" mkdir %SERVERPATH%\%DEBUGBINDIRECTORY%
 
 for /r .\include %%f in (*.h) do @copy "%%f" %SERVERPATH%\include\%TARGETDIRNAME%\
 for /r .\lib %%f in (*.lib) do @copy "%%f" %SERVERPATH%\lib\%TARGETDIRNAME%\
-for /r .\lib %%f in (*-d.dll) do @copy "%%f" %SERVERPATH%\binDebug\modules\
-for /r .\lib %%i in (*.dll) do (echo %%i | findstr /r ".*-d.dll" > nul || copy %%i %SERVERPATH%\bin\modules\)
+for /r .\lib %%f in (*-d.dll) do @copy "%%f" %SERVERPATH%\%DEBUGBINDIRECTORY%\
+for /r .\lib %%i in (*.dll) do (echo %%i | findstr /r ".*-d.dll" > nul || copy %%i %SERVERPATH%\%BINDIRECTORY%\)
