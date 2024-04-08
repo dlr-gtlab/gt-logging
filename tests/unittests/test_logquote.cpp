9d284c49041c741cd8dce63b5c482302de1c2b10
Bröcker <Marius.Broecker@dlr.de>
2024-04-08 17:18:12 +0200

    added iomanips for space and quotations

--- tests/unittests/test_logquote.cpp
+++ tests/unittests/test_logquote.cpp
@@ -12,14 +12,16 @@ class LogQuote : public LogHelperTest
 
 TEST_F(LogQuote, logQString)
 {
-    gtWarning() << "Hello" << QString{"World"};
-    EXPECT_TRUE(log.contains("Hello \"World\""));
+    gtWarning() << "Hello" << QString{"World"}
+                << gt::log::noquote << QString{"!"};
+    EXPECT_TRUE(log.contains("Hello \"World\" !"));
 }
 
 TEST_F(LogQuote, logQChar)
 {
-    gtWarning() << 'T' << QChar{'E'} << 'S' << QChar{'T'};
-    EXPECT_TRUE(log.contains("T \"E\" S \"T\""));
+    gtWarning() << 'T' << QChar{'E'} << 'S' << QChar{'T'}
+                << gt::log::noquote << QChar{'!'};
+    EXPECT_TRUE(log.contains("T \"E\" S \"T\" !"));
 }
 
 // member methods should be callable as well
@@ -27,4 +29,17 @@ TEST_F(LogQuote, callMemberMethod)
 {
     gtWarning().nospace() << 'T' << QChar{'E'} << 'S' << QLatin1String{"T"};
     EXPECT_TRUE(log.contains("T\"E\"S\"T\""));
+}
+
+TEST_F(LogQuote, iomanip)
+{
+    gtWarning() << 'T' << QChar{'E'} << 'S'
+                << gt::log::nospace // will not append a space for the next message
+                << QChar{'T'}
+                << gt::log::space // will append a space for the next message
+                << gt::log::noquote // disables quotations
+                << QString{"!"}
+                << gt::log::quote // enables quotations
+                << QString{":)"};
+    EXPECT_TRUE(log.contains("T \"E\" S \"T\"! \":)\""));
 }
