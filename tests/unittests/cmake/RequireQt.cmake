e9a5c178fed0d3fa8edb2936349561f0bef93da7
Martin Siggel <martin.siggel@dlr.de>
2026-01-08 11:33:37 +0100

    Added missing license header

--- tests/unittests/cmake/RequireQt.cmake
+++ tests/unittests/cmake/RequireQt.cmake
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025, German Aerospace Center (DLR)
+# SPDX-License-Identifier: BSD-3-Clause
+
 # ==============================================================================
 # require_qt(
 #   COMPONENTS <comp>...
